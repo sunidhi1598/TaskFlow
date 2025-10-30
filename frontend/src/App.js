0a491495f438f44122f5baa60375c74fda4d8bbf
sunidhi1598 <sunidhi15k@gmail.com>
2025-10-30 13:37:17 +0530

    message

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, TaskFlow!</h1>
+      <p>React frontend is connected successfully 🎉</p>
+    </div>
+  );
+}
+
+export default App;
+
